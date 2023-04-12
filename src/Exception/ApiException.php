fbe2cb90e74c53a2b7a670f15a91b0ae03988998
zeroicq <zeroicq@gmail.com>
2023-04-12 20:13:22 +1000

    Exceptions

---
+++ src/Exception/ApiException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Zeroicq\ExampleApiClient\Exception;
+
+class ApiException extends \Exception
+{
+}
---
+++ src/Exception/ValidationException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Zeroicq\ExampleApiClient\Exception;
+
+class ValidationException extends ApiException
+{
+}
